1d205803b0818bbaf25a94bbbe3e52d96f5bb877
Daniel Jones <tortxof@gmail.com>
2015-06-23 15:24:36 -0400

    Added SECRET_KEY handling and /setup to app.py.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
+import stat
 
-from flask import Flask, render_template, session, request, redirect, url_for
+from flask import Flask, render_template, session, flash, request, redirect, url_for
 
 import database
 
@@ -8,11 +9,20 @@ db = database.Database('/data/passwords.db')
 
 app = Flask(__name__)
 
+if not os.path.isfile('/data/key'):
+    with open('/data/key', 'wb') as f:
+        f.write(os.urandom(32))
+    os.chmod('/data/key', stat.S_IREAD)
+
+with open('/data/key', 'rb') as f:
+    app.config['SECRET_KEY'] = f.read()
+
 app.config['DEBUG'] = os.environ.get('FLASK_DEBUG', 'false').lower() == 'true'
 
 @app.route('/')
 def index():
     if db.is_new():
+        flash('No users have been added yet. Add the first user.')
         return redirect(url_for('setup'))
     else:
         return render_template('index.html')
@@ -20,7 +30,9 @@ def index():
 @app.route('/setup', methods=['GET', 'POST'])
 def setup():
     if request.method == 'POST':
-        pass
+        db.new_appuser(request.form.to_dict())
+        flash('Added user.')
+        return redirect(url_for('index'))
     else:
         return render_template('setup.html')
 
