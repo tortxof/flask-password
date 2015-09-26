52f8dd4141059c5bcf47cc61a6f23fd356801aa6
Daniel Jones <tortxof@gmail.com>
2015-09-26 15:29:34 -0400

    Made markov.py an importable class.

--- markov.py
+++ markov.py
@@ -20,31 +20,29 @@ def pairwise(iterable):
         yield a, b
         a = b
 
-with open('corpus.txt') as f:
-    corpus = f.read()
+class Markov(object):
+    def __init__(self, corpus_file='corpus.txt'):
+        with open(corpus_file) as f:
+            corpus = f.read()
+        corpus = ''.join(c.lower() if c in string.ascii_letters else ' ' for c in corpus).split()
+        corpus = [word for word in corpus if len(word) > 1]
+        self.counts = defaultdict(lambda: defaultdict(int))
+        for word in corpus:
+            for a, b in pairwise(word):
+                self.counts[a][b] += 1
+        self.sys_rand = random.SystemRandom()
 
-corpus = ''.join(c.lower() if c in string.ascii_letters else ' ' for c in corpus).split()
-corpus = [word for word in corpus if len(word) > 1]
-counts = defaultdict(lambda: defaultdict(int))
-for word in corpus:
-    for a, b in pairwise(word):
-        counts[a][b] += 1
-
-sys_rand = random.SystemRandom()
-
-def gen_password(l=16):
-    password = []
-    state = sys_rand.choice(string.ascii_lowercase)
-    password.append(state)
-    while len(password) < l:
-        choices, weights = zip(*counts[state].items())
-        cumdist = list(itertools.accumulate(weights))
-        x = sys_rand.random() * cumdist[-1]
-        state = choices[bisect.bisect(cumdist, x)]
+    def gen_password(self, l=16):
+        password = []
+        state = self.sys_rand.choice(string.ascii_lowercase)
         password.append(state)
-    cap, num = sys_rand.sample(range(len(password)), 2)
-    password[num] = str(sys_rand.randrange(10))
-    password[cap] = password[cap].upper()
-    return ''.join(password)
-
-print(gen_password())
+        while len(password) < l:
+            choices, weights = zip(*self.counts[state].items())
+            cumdist = list(itertools.accumulate(weights))
+            x = self.sys_rand.random() * cumdist[-1]
+            state = choices[bisect.bisect(cumdist, x)]
+            password.append(state)
+        cap, num = self.sys_rand.sample(range(len(password)), 2)
+        password[num] = str(self.sys_rand.randrange(10))
+        password[cap] = password[cap].upper()
+        return ''.join(password)
