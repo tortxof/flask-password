c01b22892e432fd8d0468322c62a1e463f882abe
Daniel Jones <tortxof@gmail.com>
2017-11-19 14:15:16 -0500

    Prettier reformatted js
    Also removed jquery document ready wrapper.

--- static/js/app.js
+++ static/js/app.js
@@ -1,82 +1,70 @@
-$(document).ready(function() {
-  // Workaround for clipboard.js in bootstrap modal.
-  $.fn.modal.Constructor.prototype.enforceFocus = function() {}
+// Workaround for clipboard.js in bootstrap modal.
+$.fn.modal.Constructor.prototype.enforceFocus = function() {}
 
-  $('#newusername').on('change keyup', function() {
-    var username = $(this).val()
-    $.getJSON(username_available_endpoint, { user: username }, function(data) {
-      if (data.available) {
-        $('#newusername')
-          .parent()
-          .addClass('has-success has-feedback')
-          .removeClass('has-error')
-          .find('span')
-          .addClass('glyphicon-ok')
-          .removeClass('glyphicon-remove')
-      } else {
-        $('#newusername')
-          .parent()
-          .addClass('has-error has-feedback')
-          .removeClass('has-success')
-          .find('span')
-          .addClass('glyphicon-remove')
-          .removeClass('glyphicon-ok')
-      }
-    })
-  })
-
-  function showTooltip(elem) {
-    var success_elem = document.createElement('div')
-    success_elem.className = 'cb-copy-success'
-    success_elem.textContent = 'Copied'
-    $(elem).append(success_elem)
-    $(success_elem)
-      .delay(1000)
-      .fadeOut(500, function() {
-        success_elem.parentNode.removeChild(success_elem)
-      })
-  }
-
-  var passwords_template = Handlebars.compile($('#passwords_template').html())
-  function renderPasswords() {
-    $.getJSON(generate_passwords_json_endpoint, function(pw_json) {
-      var passwords_html = passwords_template(pw_json.passwords)
-      passwords_html += passwords_template(pw_json.pins)
-      passwords_html += passwords_template(pw_json.keys)
-      passwords_html += passwords_template(pw_json.phrases)
-      $('#genpw-modal-body').html(passwords_html)
-    })
-  }
-  $('#genpw-open').click(function(evt) {
-    evt.preventDefault()
-    renderPasswords()
-    $('#genpw-modal').modal()
-  })
-  $('#genpw-refresh').click(renderPasswords)
-
-  var clipboard = new Clipboard('.cb-copy')
-  clipboard.on('success', function(e) {
-    showTooltip(e.trigger)
-  })
-
-  $('.show-pw').mousedown(function() {
-    var password = $(this)
-      .prev('.cb-copy')
-      .data('clipboard-text')
-    $(this)
-      .next('.password')
-      .text(password)
-  })
-
-  $('.show-pw').mouseup(function() {
-    var bullets = ''
-    for (var i = 0; i < 8; i++) {
-      bullets += '&bullet;'
+$('#newusername').on('change keyup', function() {
+  var username = $(this).val()
+  $.getJSON(username_available_endpoint, { user: username }, function(data) {
+    if (data.available) {
+      $('#newusername')
+        .parent()
+        .addClass('has-success has-feedback')
+        .removeClass('has-error')
+        .find('span')
+        .addClass('glyphicon-ok')
+        .removeClass('glyphicon-remove')
+    } else {
+      $('#newusername')
+        .parent()
+        .addClass('has-error has-feedback')
+        .removeClass('has-success')
+        .find('span')
+        .addClass('glyphicon-remove')
+        .removeClass('glyphicon-ok')
     }
-    $(this)
-      .next('.password')
-      .html(bullets)
   })
+})
+
+function showTooltip(elem) {
+  var success_elem = document.createElement('div')
+  success_elem.className = 'cb-copy-success'
+  success_elem.textContent = 'Copied'
+  $(elem).append(success_elem)
+  $(success_elem)
+    .delay(1000)
+    .fadeOut(500, function() {
+      success_elem.parentNode.removeChild(success_elem)
+    })
+}
+
+var passwords_template = Handlebars.compile($('#passwords_template').html())
+function renderPasswords() {
+  $.getJSON(generate_passwords_json_endpoint, function(pw_json) {
+    var passwords_html = passwords_template(pw_json.passwords)
+    passwords_html += passwords_template(pw_json.pins)
+    passwords_html += passwords_template(pw_json.keys)
+    passwords_html += passwords_template(pw_json.phrases)
+    $('#genpw-modal-body').html(passwords_html)
+  })
+}
+$('#genpw-open').click(function(evt) {
+  evt.preventDefault()
+  renderPasswords()
+  $('#genpw-modal').modal()
+})
+$('#genpw-refresh').click(renderPasswords)
+
+var clipboard = new Clipboard('.cb-copy')
+clipboard.on('success', function(e) {
+  showTooltip(e.trigger)
+})
+
+$('.show-pw').mousedown(function() {
+  var password = $(this)
+    .prev('.cb-copy')
+    .data('clipboard-text')
+  $(this)
+    .next('.password')
+    .text(password)
 })
 
 $('.show-pw').mouseup(function() {
