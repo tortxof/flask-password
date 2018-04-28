e64d2c49f3e0c359d5e6b7e2967bfad9c6675068
Daniel Jones <tortxof@gmail.com>
2018-04-28 11:44:36 -0400

    Prettier reformat

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,8 @@
 // Workaround for clipboard.js in bootstrap modal.
-$.fn.modal.Constructor.prototype.enforceFocus = function() {}
+$.fn.modal.Constructor.prototype.enforceFocus = function() {};
 
 $('#newusername').on('change keyup', function() {
-  var username = $(this).val()
+  var username = $(this).val();
   $.getJSON(username_available_endpoint, { user: username }, function(data) {
     if (data.available) {
       $('#newusername')
@@ -11,7 +11,7 @@ $('#newusername').on('change keyup', function() {
         .removeClass('has-error')
         .find('span')
         .addClass('glyphicon-ok')
-        .removeClass('glyphicon-remove')
+        .removeClass('glyphicon-remove');
     } else {
       $('#newusername')
         .parent()
@@ -19,90 +19,90 @@ $('#newusername').on('change keyup', function() {
         .removeClass('has-success')
         .find('span')
         .addClass('glyphicon-remove')
-        .removeClass('glyphicon-ok')
+        .removeClass('glyphicon-ok');
     }
-  })
-})
+  });
+});
 
 function showTooltip(elem) {
-  var success_elem = document.createElement('div')
-  success_elem.className = 'cb-copy-success'
-  success_elem.textContent = 'Copied'
-  $(elem).append(success_elem)
+  var success_elem = document.createElement('div');
+  success_elem.className = 'cb-copy-success';
+  success_elem.textContent = 'Copied';
+  $(elem).append(success_elem);
   $(success_elem)
     .delay(1000)
     .fadeOut(500, function() {
-      success_elem.parentNode.removeChild(success_elem)
-    })
+      success_elem.parentNode.removeChild(success_elem);
+    });
 }
 
-var passwords_template = Handlebars.compile($('#passwords_template').html())
+var passwords_template = Handlebars.compile($('#passwords_template').html());
 function renderPasswords() {
   $.getJSON(generate_passwords_json_endpoint, function(pw_json) {
-    var passwords_html = passwords_template(pw_json.passwords)
-    passwords_html += passwords_template(pw_json.pins)
-    passwords_html += passwords_template(pw_json.keys)
-    passwords_html += passwords_template(pw_json.phrases)
-    $('#genpw-modal-body').html(passwords_html)
-  })
+    var passwords_html = passwords_template(pw_json.passwords);
+    passwords_html += passwords_template(pw_json.pins);
+    passwords_html += passwords_template(pw_json.keys);
+    passwords_html += passwords_template(pw_json.phrases);
+    $('#genpw-modal-body').html(passwords_html);
+  });
 }
 $('#genpw-open').click(function(evt) {
-  evt.preventDefault()
-  renderPasswords()
-  $('#genpw-modal').modal()
-})
-$('#genpw-refresh').click(renderPasswords)
+  evt.preventDefault();
+  renderPasswords();
+  $('#genpw-modal').modal();
+});
+$('#genpw-refresh').click(renderPasswords);
 
-var clipboard = new Clipboard('.cb-copy')
+var clipboard = new Clipboard('.cb-copy');
 clipboard.on('success', function(e) {
-  showTooltip(e.trigger)
-})
+  showTooltip(e.trigger);
+});
 
 $('.show-pw').mousedown(function() {
   var password = $(this)
     .prev('.cb-copy')
-    .data('clipboard-text')
+    .data('clipboard-text');
   $(this)
     .next('.password')
-    .text(password)
-})
+    .text(password);
+});
 
 $('.show-pw').mouseup(function() {
-  var bullets = ''
+  var bullets = '';
   for (var i = 0; i < 8; i++) {
-    bullets += '&bullet;'
+    bullets += '&bullet;';
   }
   $(this)
     .next('.password')
-    .html(bullets)
-})
+    .html(bullets);
+});
 
-var pageload_time = new Date().getTime() / 1000
-var elSessionTimeFg = document.querySelector('.session-time .fg')
+var pageload_time = new Date().getTime() / 1000;
+var elSessionTimeFg = document.querySelector('.session-time .fg');
 
 function updateSessionCountdown() {
-  var seconds_elapsed = new Date().getTime() / 1000 - pageload_time
-  var fraction_time_left = (refresh_time - seconds_elapsed) / total_time
-  elSessionTimeFg.style.strokeDashoffset = -63 * (1 - fraction_time_left)
+  var seconds_elapsed = new Date().getTime() / 1000 - pageload_time;
+  var fraction_time_left = (refresh_time - seconds_elapsed) / total_time;
+  elSessionTimeFg.style.strokeDashoffset = -63 * (1 - fraction_time_left);
 }
 
 if (refresh_time !== null) {
-  updateSessionCountdown()
-  document.querySelector('.session-time').style.display = 'block'
+  updateSessionCountdown();
+  document.querySelector('.session-time').style.display = 'block';
   var session_countdown_interval = window.setInterval(
     updateSessionCountdown,
     1000
-  )
+  );
   window.setTimeout(function() {
-    window.clearInterval(session_countdown_interval)
-  }, refresh_time * 1000)
+    window.clearInterval(session_countdown_interval);
+  }, refresh_time * 1000);
   window.setTimeout(function() {
-    window.location.reload(true)
-  }, (refresh_time + 10) * 1000)
+    window.location.reload(true);
+  }, (refresh_time + 10) * 1000);
 }
 
 document.querySelectorAll('.record-date').forEach(function(el) {
   el.innerText = moment
     .duration(-parseInt(el.dataset.seconds), 'seconds')
-    .humanize(true)
-})
+    .humanize(true);
+});
