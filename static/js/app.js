c4430817f1ca2c570969c4ece517bc9d44add0ad
Daniel Jones <tortxof@gmail.com>
2025-03-03 00:05:34 -0500

    Format app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,99 +1,99 @@
 // Workaround for clipboard.js in bootstrap modal.
-$.fn.modal.Constructor.prototype.enforceFocus = function() {};
+$.fn.modal.Constructor.prototype.enforceFocus = function () {};
 
-$('#newusername').on('change keyup', function() {
+$("#newusername").on("change keyup", function () {
   var username = $(this).val();
-  $.getJSON(username_available_endpoint, { user: username }, function(data) {
+  $.getJSON(username_available_endpoint, { user: username }, function (data) {
     if (data.available) {
-      $('#newusername')
+      $("#newusername")
         .parent()
-        .addClass('has-success has-feedback')
-        .removeClass('has-error')
-        .find('span')
-        .addClass('glyphicon-ok')
-        .removeClass('glyphicon-remove');
+        .addClass("has-success has-feedback")
+        .removeClass("has-error")
+        .find("span")
+        .addClass("glyphicon-ok")
+        .removeClass("glyphicon-remove");
     } else {
-      $('#newusername')
+      $("#newusername")
         .parent()
-        .addClass('has-error has-feedback')
-        .removeClass('has-success')
-        .find('span')
-        .addClass('glyphicon-remove')
-        .removeClass('glyphicon-ok');
+        .addClass("has-error has-feedback")
+        .removeClass("has-success")
+        .find("span")
+        .addClass("glyphicon-remove")
+        .removeClass("glyphicon-ok");
     }
   });
 });
 
 function showTooltip(elem) {
-  var success_elem = document.createElement('div');
-  success_elem.className = 'cb-copy-success';
-  success_elem.textContent = 'Copied';
+  var success_elem = document.createElement("div");
+  success_elem.className = "cb-copy-success";
+  success_elem.textContent = "Copied";
   $(elem).append(success_elem);
   $(success_elem)
     .delay(1000)
-    .fadeOut(500, function() {
+    .fadeOut(500, function () {
       success_elem.parentNode.removeChild(success_elem);
     });
 }
 
-var passwords_template = Handlebars.compile($('#passwords_template').html());
+var passwords_template = Handlebars.compile($("#passwords_template").html());
 function renderPasswords() {
-  $.getJSON(generate_passwords_json_endpoint, function(pw_json) {
+  $.getJSON(generate_passwords_json_endpoint, function (pw_json) {
     var passwords_html = passwords_template(pw_json.passwords);
     passwords_html += passwords_template(pw_json.pins);
     passwords_html += passwords_template(pw_json.keys);
     passwords_html += passwords_template(pw_json.phrases);
-    $('#genpw-modal-body').html(passwords_html);
+    $("#genpw-modal-body").html(passwords_html);
   });
 }
-$('#genpw-open').click(function(evt) {
+$("#genpw-open").click(function (evt) {
   evt.preventDefault();
   renderPasswords();
-  $('#genpw-modal').modal();
+  $("#genpw-modal").modal();
 });
-$('#genpw-refresh').click(renderPasswords);
+$("#genpw-refresh").click(renderPasswords);
 
-var clipboard = new Clipboard('.cb-copy');
-clipboard.on('success', function(e) {
+var clipboard = new Clipboard(".cb-copy");
+clipboard.on("success", function (e) {
   showTooltip(e.trigger);
 });
 
 function showPassword(e) {
-  if (e.target.classList.contains('show-pw')) {
+  if (e.target.classList.contains("show-pw")) {
     e.preventDefault();
-    var password = e.target.parentElement.querySelector('button.cb-copy')
-      .dataset.clipboardText;
-    e.target.parentElement.querySelector('.password').innerText = password;
+    var password =
+      e.target.parentElement.querySelector("button.cb-copy").dataset
+        .clipboardText;
+    e.target.parentElement.querySelector(".password").innerText = password;
   }
 }
 
 function hidePassword(e) {
-  if (e.target.classList.contains('show-pw')) {
+  if (e.target.classList.contains("show-pw")) {
     e.preventDefault();
-    e.target.parentElement.querySelector(
-      '.password'
-    ).innerHTML = '&bullet;'.repeat(8);
+    e.target.parentElement.querySelector(".password").innerHTML =
+      "&bullet;".repeat(8);
   }
 }
 
 document
-  .getElementById('main-container')
-  .addEventListener('mousedown', showPassword);
+  .getElementById("main-container")
+  .addEventListener("mousedown", showPassword);
 
 document
-  .getElementById('main-container')
-  .addEventListener('touchstart', showPassword);
+  .getElementById("main-container")
+  .addEventListener("touchstart", showPassword);
 
 document
-  .getElementById('main-container')
-  .addEventListener('mouseup', hidePassword);
+  .getElementById("main-container")
+  .addEventListener("mouseup", hidePassword);
 
 document
-  .getElementById('main-container')
-  .addEventListener('touchend', hidePassword);
+  .getElementById("main-container")
+  .addEventListener("touchend", hidePassword);
 
 var pageload_time = new Date().getTime() / 1000;
-var elSessionTimeFg = document.querySelector('.session-time .fg');
+var elSessionTimeFg = document.querySelector(".session-time .fg");
 
 function updateSessionCountdown() {
   var seconds_elapsed = new Date().getTime() / 1000 - pageload_time;
@@ -103,21 +103,21 @@ function updateSessionCountdown() {
 
 if (refresh_time !== null) {
   updateSessionCountdown();
-  document.querySelector('.session-time').style.display = 'block';
+  document.querySelector(".session-time").style.display = "block";
   var session_countdown_interval = window.setInterval(
     updateSessionCountdown,
     1000
   );
-  window.setTimeout(function() {
+  window.setTimeout(function () {
     window.clearInterval(session_countdown_interval);
   }, refresh_time * 1000);
-  window.setTimeout(function() {
+  window.setTimeout(function () {
     window.location.reload(true);
   }, (refresh_time + 10) * 1000);
 }
 
-document.querySelectorAll('.record-date').forEach(function(el) {
+document.querySelectorAll(".record-date").forEach(function (el) {
   el.innerText = moment
-    .duration(-parseInt(el.dataset.seconds), 'seconds')
+    .duration(-parseInt(el.dataset.seconds), "seconds")
     .humanize(true);
 });
